2079bd6cd5568f0bf76218b2293d4e1fdccd683f
akashkuchankar <kuchankarakash@gmail.com>
2022-10-12 00:48:17 -0700

    Add project files.

---
+++ employee/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager m1 = new Manager("man1", 23000, 40000);
+            m1.CalculateSalary();
+            Console.WriteLine(m1); ;
+
+            president p1 = new president("president1", 23000, 34444,5466);
+            p1.CalculateSalary();
+            Console.WriteLine(p1);
+
+            SalesManager sm1 = new SalesManager("vikas", 34000, 349, 879);
+            sm1.CalculateSalary();
+            Console.WriteLine(sm1);
+        }
+    }
+}
---
+++ employee/employee1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace employee
+{
+    public class Employee1
+    {
+        protected int id;
+        protected string name;
+        protected double basicsalary, hra, da, pf, netpaid;
+        private static int count;
+        public Employee1(string name, double basicsalary)
+        {
+            count++;
+            this.id = count;
+            this.name = name;
+            this.basicsalary = basicsalary;
+
+        }
+        public virtual void CalculateSalary()
+        {
+            hra = basicsalary * 0.40;
+            da = basicsalary * 0.20;
+            pf = basicsalary * 0.12;
+            netpaid = (hra + da + basicsalary) - pf;
+        }
+        public static string TotalCount()
+        {
+            return $"Total count is count";
+        }
+        public override string ToString()
+        {
+            return $"employee details::: Id:{id} Name{name} Salary{netpaid}";
+
+        }
+    }
+    public class Manager : Employee1
+    {
+        private double ta;
+        public Manager(string name, double bs, double ta) : base(name, bs)
+        {
+            this.ta = ta;
+        }
+        public override void CalculateSalary()
+        {
+            hra = basicsalary * 0.40;
+            da = basicsalary * 0.20;
+            pf = basicsalary * 0.12;
+            netpaid = (hra + da + basicsalary + ta) - pf;
+
+        }
+        public override string ToString()
+        {
+            return $"Manager details :::Id: {id},  Name {name},  Salary {netpaid}";
+        }
+
+    }
+    public class president : Employee1
+    {
+        private double ta;
+        private double medical;
+        public president(string name, double bs, double ta, double medical) : base(name, bs)
+        {
+            this.medical = medical;
+            this.ta = ta;
+        }
+        public override void CalculateSalary()
+        {
+            hra = basicsalary * 0.40;
+            da = basicsalary * 0.20;
+            pf = basicsalary * 0.12;
+            netpaid = (hra + da + basicsalary + ta + medical) - pf;
+
+        }
+        public override string ToString()
+        {
+            return $"President details :::Id: {id},  Name {name},  Salary {netpaid}";
+        }
+
+    }
+    public class SalesManager : Employee1
+    {
+        private double ta;
+        private double unifrom;
+
+        public SalesManager(string name, double bs, double ta, double unifrom) : base(name, bs)
+        {
+            this.unifrom = unifrom;
+            this.ta = ta;
+        }
+        public override void CalculateSalary()
+        {
+            hra = basicsalary * 0.40;
+            da = basicsalary * 0.20;
+            pf = basicsalary * 0.12;
+            netpaid = (hra + da + basicsalary + ta + unifrom) - pf;
+
+        }
+        public override string ToString()
+        {
+            return $"President details :::Id: {id},  Name {name},  Salary {netpaid}";
+        }
+
+    }
+}
